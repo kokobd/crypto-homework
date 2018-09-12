869b8f4b76154c65f9150b667b954af93db3d180
zelinf <contact@zelinf.net>
2018-09-13 07:33:48 +0800

    Implement 'VirginiaCracker.crack()'

--- src/main/java/net/zelinf/crypto_homework/classical/Ex01.java
+++ src/main/java/net/zelinf/crypto_homework/classical/Ex01.java
@@ -1,11 +1,25 @@
 package net.zelinf.crypto_homework.classical;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+
 /**
  * Crack Virginia password
  */
 public class Ex01 {
 
     public static void main(String[] args) {
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Ex01.class.getResourceAsStream("cipher.txt")))) {
+            String cipherText = reader.readLine();
+            VirginiaCracker cracker = new VirginiaCracker(cipherText);
+            Set<Integer> distances = cracker.findDistances();
+            System.out.println(distances);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- src/main/java/net/zelinf/crypto_homework/classical/VirginiaCracker.java
+++ src/main/java/net/zelinf/crypto_homework/classical/VirginiaCracker.java
@@ -1,8 +1,13 @@
 package net.zelinf.crypto_homework.classical;
 
-import java.nio.charset.Charset;
-import java.util.Arrays;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.*;
+
+/**
+ * Cracker of Virginia password.
+ * Note that this class is not thread safe.
+ */
 public class VirginiaCracker {
 
     public VirginiaCracker(String cipherText) {
@@ -11,23 +16,62 @@ public class VirginiaCracker {
 
     private byte[] cipherText;
 
-    public int calculatePeriod() {
+    public List<String> crack() {
+        return Collections.emptyList();
+    }
+
+    private List<Integer> possiblePeriods_ = null;
+
+    /**
+     * Calculate possible periods
+     *
+     * @param errors maximum count of permitted errors in 'distances'
+     * @return a list of possible periods. The most possible one comes first
+     */
+    public List<Integer> possiblePeriods(int errors) {
+        if (possiblePeriods_ != null)
+            return possiblePeriods_;
+
+        Set<Integer> distances = findDistances();
+
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        for (int errorCount = 0; errorCount <= errors; ++errorCount) {
+
+            BitSet selected = new BitSet();
+            // TODO
+        }
+
+        return Collections.emptyList();
+    }
+
+    private Set<Integer> distances;
+
+    public Set<Integer> findDistances() {
+        if (distances != null)
+            return distances;
+        distances = new HashSet<>();
         for (int len = 3; len <= cipherText.length / 2; ++len) {
             for (int i = 0; i <= cipherText.length - len; ++i) {
                 // [i, j) is the currently selected interval
                 final int j = i + len;
+                byte[] range_1 = ArrayUtils.subarray(cipherText, i, j);
 
                 // Compare interval [i, j) with all remaining
                 // intervals that have same length
-                for (int m = i + 1; m <= cipherText.length - len; ++i) {
+                for (int m = i + 1; m <= cipherText.length - len; ++m) {
                     final int n = m + len;
-
                     // Now we selected another interval [m, n)
+                    byte[] range_2 = ArrayUtils.subarray(cipherText, m, n);
 
+                    if (Arrays.equals(range_1, range_2)) {
+                        // Finally we found a 'distance'
+                        final int distance = m - i;
+                        distances.add(distance);
+                    }
                 }
             }
         }
-
-        throw new UnsupportedOperationException();
+        return distances;
     }
+
 }
